96f7acdc8a311faaef15bdf8ec4fbad95dda74c9
Víctor Saldanya <victor.saldana@heliocor.com>
2023-04-05 17:25:15 +0200

    Delete unused files

--- templates/banners/col_left_es.php
+++
@@ -1,6 +0,0 @@
-	<div id="socials_2">
-        	<a target="_blank" href="http://www.youtube.com/satanarisemetal" title="Canal de Youtube de Satan Arise"><img src="pics/social_youtube.png" width="25" height="25" alt="Canal de Youtube de Satan Arise" /></a>
-            <a target="_blank" href="http://twitter.com/SatanArise" title="Satan Arise Twitter"><img src="pics/social_twitter.png" width="25" height="25" alt="Satan Arise Twitter" /></a>
-            <a target="_blank" href="http://www.facebook.com/Satanarisemetal" title="Satan Arise Facebook"><img src="pics/social_facebook.png" width="25" height="25" alt="Satan Arise Facebook" /></a>
-    </div>				
-
