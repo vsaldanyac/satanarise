2aa2c705fbb8436eb5712d2dd86dc14eae84cc8c
Víctor Saldanya <victor.saldana@heliocor.com>
2024-04-05 12:11:25 +0200

    404 page, but unused

---
+++ pags/404.php
@@ -0,0 +1,15 @@
+
+<head>
+<meta name="description" content="Lo Mejor del 2017 - Heavy Metal."  />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link type="text/css" rel="stylesheet" media="all" href="css/discografia.css" />
+<link type="text/css" rel="stylesheet" media="all" href="css/index.css" />
+<link type="text/css" rel="stylesheet" media="all" href="css/noticias.css" />
+<link type="text/css" rel="stylesheet" media="all" href="css/disc.css" />
+<script type="text/javascript" src="css/prototype.js"></script>
+<script type="text/javascript" src="css/scriptaculous.js?load=effects,builder"></script>
+<script type="text/javascript" src="css/lightbox.js"></script>
+
+<title>404 Page Not Found Error - Satan Arise </title>
+</head>		
+404 Page Not Found Error - Satan Arise 
