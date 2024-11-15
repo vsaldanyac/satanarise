343a7de5f15e29f7d038dd114a7d4d5bc4f5ce91
Víctor Saldanya <victor.saldana@heliocor.com>
2024-11-15 12:47:35 +0100

    Gigs to 2026

--- sources/ob_cp_conciertos.php
+++ sources/ob_cp_conciertos.php
@@ -660,7 +660,7 @@ class cp_propers_concerts
 				$contingut_camp = $this->dades->$nom->any;
 			}
 
-			for ($y = 2024; $y <= 2025; $y++) {
+			for ($y = 2024; $y <= 2026; $y++) {
 				if ($contingut_camp == $y) {
 					print '<option selected="selected" value="' . $y . '">' . $y . '</option>';
 
