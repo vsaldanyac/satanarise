f0c52d1b5286ee923b12a754e05ed0424f8374bf
Víctor Saldanya <victor.saldana@heliocor.com>
2025-06-12 17:03:12 +0200

    opinio special

--- sources/ob_opinio_web.php
+++ sources/ob_opinio_web.php
@@ -39,6 +39,21 @@
 				$this->numero_resultats=$this->resultat_consulta->num_rows;
 			}
 		}
+
+				public function extreure_opinio_per_data_entrada_special($bd,$punter,$quantitat,$leng) 
+		/* consulta noticies a la bbdd a partir de la conexió, el numero d'inici de la consulta per data i la quantitat a mostrar */
+		{
+			/* Demanar dades a bbdd ordenat per data, desde $desde fins a $desde+$quantitat */
+			$inici=$punter-1;
+			
+			$query= "select idopinio, ruta, texte_es, texte_cat, titol_es, titol_cat, data from opinio where special = true and idioma = '".$leng."' or idioma = 'BOTH' order by data desc limit ".$inici.", ".$quantitat;
+            //$query= "select reviews.link, reviews.banda, reviews.disc, reviews.any, reviews.portada, estil.estil, reviews.tipus, reviews.nota, colaboradors.nom, banderes.pais, banderes.ruta from reviews left join colaboradors on reviews.idcolaboradors=colaboradors.idcolaboradors left join  banderes on reviews.idpais=banderes.idpais left join estil on reviews.idestil=estil.idestil order by reviews.data desc";	
+            $this->resultat_consulta=$bd->query($query);
+            if ($this->resultat_consulta!=FALSE) 
+			{
+				$this->numero_resultats=$this->resultat_consulta->num_rows;
+			}
+		}
 		
 		public function mostrar_opinio_per_data_entrada($bd,$leng)
 		{
